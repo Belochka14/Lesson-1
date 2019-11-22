fed13adfcaf2ba8294c2589b1c970b829803cd31
Valeria Ivinskaya <ivinskaya89@gmail.com>
2019-11-22 11:36:36 +0200

    alert change

--- script.js
+++ script.js
@@ -18,4 +18,4 @@ let a1 = prompt("Введите обязательную статью расхо
 appData.expenses[a1] = a2;
 appData.expenses[a3] = a4;
 
-alert("Ваш бюджет на 1 день составит " + appData.budget / 30);
+alert(appData.budget / 30);
