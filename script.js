5c02435afea88dc08ca4850a43de677461e3d1d2
Valeria Ivinskaya <ivinskaya89@gmail.com>
2019-12-25 17:32:26 +0200

    Lesson1/modified

--- script.js
+++ script.js
@@ -11,24 +11,48 @@ let appData = {
 };
 
 for (let i = 0; i < 2; i++) {
-  let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    b = prompt("Во сколько обойдется?", "");
+  let a = prompt("Введите обязательную статью расходов в этом месяце", "");
 
-  if (
-    typeof a === "string" &&
-    typeof a != null &&
-    typeof b != null &&
-    a != "" &&
-    b != "" &&
-    a.length < 50
+  while (
+    typeof a !== "string" ||
+    typeof a == null ||
+    a == "" ||
+    a.length > 50
   ) {
-    console.log("done");
-    appData.expenses[a] = b;
-  } else {
-    console.log("bad result");
-    i--;
+    alert("try again");
+    a = prompt("Введите обязательную статью расходов в этом месяце", "");
   }
+
+  let b = prompt("Во сколько обойдется?", "");
+
+  while (b != +b || b == null || b == "") {
+    // while (не число ИЛИ нажали "отмена" ИЛИ оставили строку пустой)
+    alert("try again");
+    b = prompt("Во сколько обойдется?", "");
+  }
+
+  appData.expenses[a] = b;
 }
+
+// for (let i = 0; i < 2; i++) {
+//   let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//     b = prompt("Во сколько обойдется?", "");
+
+//   if (
+//     typeof a === "string" &&
+//     typeof a != null &&
+//     typeof b != null &&
+//     a != "" &&
+//     b != "" &&
+//     a.length < 50
+//   ) {
+//     console.log("done");
+//     appData.expenses[a] = b;
+//   } else {
+//     console.log("bad result");
+//     i--;
+//   }
+// }
 
 // Используем цикл WHILE
 // let i = 0;
